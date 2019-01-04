b313aa0aa354064d96fe1a3cfc018803f0a2384f
Chaoshen Hsu <chaoshen_hsu@zoomnow.net>
2019-01-04 16:10:12 +0800

    test1

--- src/main/java/com/dimobo/zoom/RecordingManager/RecordingManagerApplication.java
+++ src/main/java/com/dimobo/zoom/RecordingManager/RecordingManagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecordingManagerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test1");
 		SpringApplication.run(RecordingManagerApplication.class, args);
 	}
 
