588155ca9e833b7cbc7ea9d6eebcfb023097e3e3
Chaoshen Hsu <chaoshen_hsu@zoomnow.net>
2019-01-04 15:48:57 +0800

    init

---
+++ src/main/java/com/dimobo/zoom/RecordingManager/RecordingManagerApplication.java
@@ -0,0 +1,14 @@
+package com.dimobo.zoom.RecordingManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordingManagerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/dimobo/zoom/RecordingManager/RecordingManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dimobo.zoom.RecordingManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecordingManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
